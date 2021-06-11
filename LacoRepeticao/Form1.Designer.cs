db333c5c0066f476cec90f732164105d4b5f0180
Thiago Braddock <braddock01@hotmail.com>
2021-06-10 22:35:48 -0300

    Adicionar arquivos de projeto.

---
+++ LacoRepeticao/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿
+namespace LacoRepeticao
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbResult = new System.Windows.Forms.ListBox();
+            this.btnFor = new System.Windows.Forms.Button();
+            this.btnDoWhile = new System.Windows.Forms.Button();
+            this.btnForeach = new System.Windows.Forms.Button();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFim = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbResult
+            // 
+            this.lbResult.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbResult.FormattingEnabled = true;
+            this.lbResult.ItemHeight = 21;
+            this.lbResult.Location = new System.Drawing.Point(12, 30);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(230, 340);
+            this.lbResult.TabIndex = 0;
+            // 
+            // btnFor
+            // 
+            this.btnFor.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnFor.Location = new System.Drawing.Point(258, 93);
+            this.btnFor.Name = "btnFor";
+            this.btnFor.Size = new System.Drawing.Size(130, 66);
+            this.btnFor.TabIndex = 1;
+            this.btnFor.Text = "For";
+            this.btnFor.UseVisualStyleBackColor = true;
+            this.btnFor.Click += new System.EventHandler(this.btnFor_Click);
+            // 
+            // btnDoWhile
+            // 
+            this.btnDoWhile.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnDoWhile.Location = new System.Drawing.Point(258, 165);
+            this.btnDoWhile.Name = "btnDoWhile";
+            this.btnDoWhile.Size = new System.Drawing.Size(130, 66);
+            this.btnDoWhile.TabIndex = 1;
+            this.btnDoWhile.Text = "For Com Array";
+            this.btnDoWhile.UseVisualStyleBackColor = true;
+            this.btnDoWhile.Click += new System.EventHandler(this.btnDoWhile_Click);
+            // 
+            // btnForeach
+            // 
+            this.btnForeach.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnForeach.Location = new System.Drawing.Point(258, 303);
+            this.btnForeach.Name = "btnForeach";
+            this.btnForeach.Size = new System.Drawing.Size(130, 66);
+            this.btnForeach.TabIndex = 1;
+            this.btnForeach.Text = "For com Array 2";
+            this.btnForeach.UseVisualStyleBackColor = true;
+            this.btnForeach.Click += new System.EventHandler(this.btnForeach_Click);
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtNumero.Location = new System.Drawing.Point(258, 54);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(130, 33);
+            this.txtNumero.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(258, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Digite um numero para verificar a tabuada";
+            // 
+            // txtFim
+            // 
+            this.txtFim.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtFim.Location = new System.Drawing.Point(410, 54);
+            this.txtFim.Name = "txtFim";
+            this.txtFim.Size = new System.Drawing.Size(130, 33);
+            this.txtFim.TabIndex = 2;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtSenha.Location = new System.Drawing.Point(410, 256);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(130, 33);
+            this.txtSenha.TabIndex = 4;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtUsuario.Location = new System.Drawing.Point(258, 256);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(130, 33);
+            this.txtUsuario.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(258, 238);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Usuário";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(410, 238);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Senha";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblNome.ForeColor = System.Drawing.Color.Coral;
+            this.lblNome.Location = new System.Drawing.Point(12, 391);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(0, 32);
+            this.lblNome.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 483);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFim);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.btnForeach);
+            this.Controls.Add(this.btnDoWhile);
+            this.Controls.Add(this.btnFor);
+            this.Controls.Add(this.lbResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbResult;
+        private System.Windows.Forms.Button btnFor;
+        private System.Windows.Forms.Button btnDoWhile;
+        private System.Windows.Forms.Button btnForeach;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFim;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label lblNome;
+    }
+}
+
---
+++ LacoRepeticao/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LacoRepeticao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFor_Click(object sender, EventArgs e)
+        {
+            int numero = int.Parse(txtNumero.Text);
+            int fim = int.Parse(txtFim.Text);
+            if(numero > fim)
+            {
+                MessageBox.Show("O 1º valor precisa ser menor que o 2º");
+                return;
+            }
+
+            lbResult.Items.Clear();
+
+            for (int fator = numero; fator <= fim; fator++)
+            {
+                lbResult.Items.Add("Tabuada do " + fator);
+                for (int i = 0; i < 11; i++)
+                {
+                    lbResult.Items.Add($" {fator} X {i} = {fator*i}");
+                }
+                lbResult.Items.Add("");
+            }
+
+        }
+
+        private void btnDoWhile_Click(object sender, EventArgs e)
+        {
+            string[] animais = {"gato", "cachorro", "cobra", "cavalo", "coelho", "dinossauro"};
+          
+            for (int i = 0; i< animais.Length; i++)
+            {
+                lbResult.Items.Add(animais[i]);
+            }
+        }
+
+        private void btnForeach_Click(object sender, EventArgs e)
+        {
+            string[,] usuarios = {
+                    {"Isadora", "usuario0", "123"},
+                    {"Carlos",  "usuario1", "123"},
+                    {"William", "usuario2", "123"},
+                    {"João",    "usuario3", "123"},
+                    {"Eder",    "usuario4", "123"},
+                    {"Julio",   "usuario5", "123"}
+            };
+
+            lbResult.Items.Add("QTD DE ITENS NO ARRAY");
+            lbResult.Items.Add(usuarios.GetLength(0));
+
+            lbResult.Items.Add("QTD DE COLUNAS NO ARRAY");
+            lbResult.Items.Add(usuarios.GetLength(1));
+
+            lbResult.Items.Add("NOME DO 1º USUARIO NO ARRAY");
+            lbResult.Items.Add(usuarios[0,0]);
+
+            lbResult.Items.Add("NOME DO ULTIMO USUARIO NO ARRAY");
+            lbResult.Items.Add(usuarios[usuarios.GetLength(0)-1, 0]);
+
+            lbResult.Items.Add("TODOS OS USUARIOS CADASTRADOS");
+            for(int i = 0; i< usuarios.GetLength(0); i++ )
+            {
+                lbResult.Items.Add(i + " - " + usuarios[i,0] + " / " + usuarios[i, 1]);
+            }
+
+            string usuario = txtUsuario.Text;
+            string senha = txtSenha.Text;
+
+            for(int i = 0; i < usuarios.GetLength(0); i++)
+            {
+                if(usuario == usuarios[i,1] && senha == usuarios[i,2])
+                {
+                    //MessageBox.Show("Usuario Encontrado Uhul, Boa Noite " + usuarios[i, 0].ToUpper());
+                    lblNome.Text = "Usuario Encontrado Uhul, Boa Noite " + usuarios[i, 0].ToUpper();
+                    return;
+                }
+            }
+
+            MessageBox.Show("Usuario ou senha incorretos");
+        }
+    }
+}
